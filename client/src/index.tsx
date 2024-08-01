f7da512bdd9e40545ae69d24b4dccf787372c34e
wladyDe <vovawestland@gmail.com>
2024-08-01 12:42:03 +0200

    define react routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { store } from './app/store';
 import './index.css';
+import { Paths } from './paths';
+import { Login } from './pages/login';
+import { Register } from './pages/register';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Employees</h1>
+  },
+  {
+    path: Paths.login,
+    element: <Login/>
+  },
+  {
+    path: Paths.register,
+    element: <Register/>
+  }
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -10,7 +29,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>asdf</h1>
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
