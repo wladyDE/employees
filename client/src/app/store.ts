7e5739914c7e30b27245f1ea3c67ca94e9f7840b
wladyDe <vovawestland@gmail.com>
2024-08-15 16:31:10 +0200

    add employees reducer

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,12 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import auth from '../features/auth/authSlice'
+import employees from '../features/employees/employeesSlice'
 import {api} from './services/api'
 import { listenerMiddleware } from '../middleware/auth';
+import { employeesApi } from './services/employees';
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath] : api.reducer,
     auth,
+    employees
   },
   middleware: (getDefaultMiddleware) => 
    getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware)
