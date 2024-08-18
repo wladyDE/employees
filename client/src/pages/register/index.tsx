a7e759dc33cf1a5e9fb2a37334f21bb99d1c870a
wladyDe <vovawestland@gmail.com>
2024-08-18 10:15:32 +0200

    add registration

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -3,16 +3,48 @@ import Layout from '../../components/layout'
 import { Card, Row, Form, Space, Typography } from 'antd'
 import { PasswordInput } from '../../components/password-input/input'
 import { CustomButton } from '../../components/custom-button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Paths } from '../../paths'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../features/auth/authSlice'
+import { useState } from 'react'
+import { useRegisterMutation } from '../../app/services/auth'
+import { User } from '@prisma/client'
+import { isErrorWithMessage } from '../../utils/is-error-with-message'
+import ErrorMessage from '../../components/error-message'
+
+type RegisterData = Omit<User, 'id'> & { confirmPassword: string }
 
 export const Register = () => {
+  const navigate = useNavigate()
+  const user = useSelector(selectUser)
+  const [error, setError] = useState('')
+  const [registerUser] = useRegisterMutation()
+
+  const register = async (data: RegisterData) => {
+    console.log(data);
+    
+    try {
+      await registerUser(data).unwrap()
+
+      navigate('/')
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err)
+
+      if (maybeError) {
+        setError(err.data.message)
+      } else {
+        setError('Uknown error')
+      }
+    }
+  }
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title='Register' style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
-            <CustomInput name='email' placeholder='Name' />
+          <Form onFinish={register}>
+            <CustomInput name='name' placeholder='Name' />
             <CustomInput type='email' name='email' placeholder='Email' />
             <PasswordInput name='password' placeholder='Password' />
             <PasswordInput name='confirmPassword' placeholder='Confirm password' />
@@ -27,6 +59,7 @@ export const Register = () => {
             <Typography.Text>
               Already registered? <Link to={Paths.login}>Login</Link>
             </Typography.Text>
+            <ErrorMessage message={error}/>
           </Space>
         </Card>
       </Row>
